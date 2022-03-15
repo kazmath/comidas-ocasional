fd122c0a06abd70d9cd255c3a7145cbef14713da
Martuzam <100171327+Martuzam@users.noreply.github.com>
2022-03-15 19:34:43 -0300

    alteraçao
    feita em ifood

--- src/Ifood.java
+++ src/Ifood.java
@@ -1,8 +1,25 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ifood {
   ArrayList<Servico> pedidos = new ArrayList<Servico>();
   public static void main (String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int escolha;
+
+
+    System.out.println("Bem-Vindo ao Comidas Ocasionais");
+    System.out.println("""
+    =========================================
+    1-Login
+    2-Cadastro de Usuario
+    3-Cadastro de Restaurante
+    =========================================
+    """);
+    System.out.println("escolha as opçoes:");
+    escolha = scan.nextInt();
+
+    
 	
   }
 }
