cc9f768823986c50b94aa569fc4778869c79c175
Juudoido <84882813+Juudoido@users.noreply.github.com>
2022-03-21 18:54:08 -0300

    Adição de Getters e Setters

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,10 +3,26 @@ public class Cliente {
     Endereco endereco;
 
     public Cliente(
-        Cadastro cadastro,
-        Endereco endereco
-    ) {
+            Cadastro cadastro,
+            Endereco endereco) {
         this.cadastro = cadastro;
         this.endereco = endereco;
     }
+
+    public Cadastro getCadastro() {
+        return this.cadastro;
+    }
+
+    public void setCadastro(Cadastro cadastro) {
+        this.cadastro = cadastro;
+    }
+
+    public Endereco getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
 }
--- src/Estabelecimento.java
+++ src/Estabelecimento.java
@@ -6,22 +6,69 @@ public class Estabelecimento {
     Calendar abertura;
     Calendar fechamento;
     Endereco endereco;
-    HashMap<String,Integer> menu;
+    HashMap<String, Integer> menu;
     String nome;
 
     public Estabelecimento(
-        String nome,
-        String cnpj,
-        Calendar abertura,
-        Calendar fechamento,
-        Endereco endereco,
-        HashMap<String,Integer> menu
-    ) {
+            String nome,
+            String cnpj,
+            Calendar abertura,
+            Calendar fechamento,
+            Endereco endereco,
+            HashMap<String, Integer> menu) {
         this.nome = nome;
         this.cnpj = cnpj;
         this.abertura = abertura;
         this.fechamento = fechamento;
         this.endereco = endereco;
         this.menu = menu;
+    }
+
+    public String getCnpj() {
+        return this.cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public Calendar getAbertura() {
+        return this.abertura;
+    }
+
+    public void setAbertura(Calendar abertura) {
+        this.abertura = abertura;
+    }
+
+    public Calendar getFechamento() {
+        return this.fechamento;
+    }
+
+    public void setFechamento(Calendar fechamento) {
+        this.fechamento = fechamento;
+    }
+
+    public Endereco getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public HashMap<String, Integer> getMenu() {
+        return this.menu;
+    }
+
+    public void setMenu(HashMap<String, Integer> menu) {
+        this.menu = menu;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 }
