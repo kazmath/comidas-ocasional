3f987cbabab0ed451eb733a8db681dbdd9ba707c
Kazuma-Chan <66137568+Kazuma-chan@users.noreply.github.com>
2022-05-25 00:59:09 -0300

    Database.sql: agora vai

--- src/main/Database.sql
+++ src/main/Database.sql
@@ -9,22 +9,6 @@ CREATE TABLE Cadastro (
     Senha VARCHAR(255) NOT NULL,
     PRIMARY KEY (CodCadastro)
 );
- 
-CREATE TABLE Estabelecimento (
-    CodEstabelecimento INT NOT NULL AUTO_INCREMENT,
-    Nome VARCHAR(11) NOT NULL,
-    CNPJ VARCHAR(11) NOT NULL,
-    Horario INT NOT NULL,
-    Aberto BINARY NOT NULL,
-    PRIMARY KEY (CodEstabelecimento)
-);
-
-CREATE TABLE Cardapio (
-    fk_Estabelecimento_Cod INT NOT NULL,
-    Prato_Id INT NOT NULL,
-    FOREIGN KEY (fk_Estabelecimento_Cod) REFERENCES Estabelecimento (CodEstabelecimento),
-    FOREIGN KEY (Prato_Id) REFERENCES Prato (Id)
-);
 
 CREATE TABLE Prato (
     Id INT NOT NULL AUTO_INCREMENT,
@@ -42,32 +26,54 @@ CREATE TABLE Endereco (
     PRIMARY KEY (CodEndereco)
 );
 
-CREATE TABLE Pedido (
-    fk_Pagamento_Pagamento_PK INT,
-    Retira_Entrega BINARY,
-    FOREIGN KEY (fk_Pagamento_Pagamento_PK) REFERENCES Pagamento (Pagamento_PK)
-);
-
-CREATE TABLE Carrinho_Aplicativo (
-    Quantidade_Item INT,
-    Prato_Id INT,
-    FOREIGN KEY (Prato_Id) REFERENCES Prato(Id)
-);
-
 CREATE TABLE Pagamento (
     Pagamento_PK INT NOT NULL,
     Pagamento INT,
     PRIMARY KEY (Pagamento_PK)
 );
+ 
+CREATE TABLE Estabelecimento (
+    CodEstabelecimento INT NOT NULL AUTO_INCREMENT,
+    Nome VARCHAR(11) NOT NULL,
+    CNPJ VARCHAR(11) NOT NULL,
+    Horario INT NOT NULL,
+    Aberto BINARY NOT NULL,
+    CodEndereco INT NOT NULL,
+    FOREIGN KEY (CodEndereco) REFERENCES Endereco(CodEndereco),
+    PRIMARY KEY (CodEstabelecimento),
+);
 
 CREATE TABLE Cliente_PF (
     Nome VARCHAR(255),
     CPF VARCHAR(11),
+    CodEndereco INT NOT NULL,
+    FOREIGN KEY (CodEndereco) REFERENCES Endereco(CodEndereco),
     PRIMARY KEY (Pagamento_PK)
 );
 
 CREATE TABLE Cliente_PJ (
     Nome VARCHAR(255),
     CNPJ VARCHAR(11),
+    CodEndereco INT NOT NULL,
+    FOREIGN KEY (CodEndereco) REFERENCES Endereco(CodEndereco),
     PRIMARY KEY (CNPJ)
+);
+
+CREATE TABLE Cardapio (
+    fk_Estabelecimento_Cod INT NOT NULL,
+    Prato_Id INT NOT NULL,
+    FOREIGN KEY (fk_Estabelecimento_Cod) REFERENCES Estabelecimento (CodEstabelecimento),
+    FOREIGN KEY (Prato_Id) REFERENCES Prato (Id)
+);
+
+CREATE TABLE Pedido (
+    FK_Pagamento INT,
+    Retira_Entrega BINARY,
+    FOREIGN KEY (FK_Pagamento) REFERENCES Pagamento (Pagamento_PK)
+);
+
+CREATE TABLE Carrinho_Aplicativo (
+    Quantidade_Item INT,
+    Prato_Id INT,
+    FOREIGN KEY (Prato_Id) REFERENCES Prato(Id)
 );
