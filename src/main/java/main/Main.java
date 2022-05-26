515a05ca0545af2604d59ef0cac0e787dcc90af4
Kazuma-Chan <66137568+Kazuma-chan@users.noreply.github.com>
2022-05-26 15:31:11 -0300

    Consertar `import` estranho

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-import com.mysql.cj.protocol.Resultset;
+import java.sql.ResultSet;
 
 import java.io.BufferedReader;
 import java.io.File;
